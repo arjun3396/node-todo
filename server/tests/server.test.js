1965544ef8da495f25d7fc44130d1287af263641
Arjun Vedak <arjun.vedak07@hotmail.com>
2018-11-19 15:50:28 +0530

    add test for get/todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,19 @@ const request = require('supertest');
 const {app} = require('../server');
 const {Todo} = require('./../models/todo');
 
+var todos = [{
+  text : "first test todo"
+},{
+  text : "Second test todo"
+}];
+
 beforeEach((done) => {
   Todo.remove({})
-    .then(done());
+    .then(() => {
+        return Todo.insertMany(todos)
+    }).then(() => done());
 });
+
 describe('Post /todos', () => {
   it('Should create a new todo', (done) => {
     var text = 'Test todo text';
@@ -24,7 +33,7 @@ describe('Post /todos', () => {
           done(err);
         }
 
-        Todo.find()
+        Todo.find({text})
           .then((todos) => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -46,10 +55,22 @@ describe('Post /todos', () => {
 
         Todo.find()
           .then((todos) => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch((e) => done(e));
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('Should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect((res.body.todos.length).toBe(2));
+      })
+      .end(done());
   });
 });
