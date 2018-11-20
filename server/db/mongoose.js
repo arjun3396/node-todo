eab13d758307248d057da021141737b1a3ac68ce
Arjun Vedak <arjun.vedak07@hotmail.com>
2018-11-20 09:04:56 +0530

    port changed for mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-
+export MONGOLAB_URI= 'mongodb://admin:useadmin@ds245512.mlab.com:45512/testing';
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
 
