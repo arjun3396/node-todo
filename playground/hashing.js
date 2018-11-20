a4fb43ac7b9cd02e4e3536143aa87ac9aa27d2d5
Arjun Vedak <arjun.vedak07@hotmail.com>
2018-11-20 19:27:50 +0530

    added auth

---
+++ playground/hashing.js
@@ -0,0 +1,38 @@
+const {SHA256} = require('crypto-js');
+
+const jwt = require('jsonwebtoken');
+
+var data = {
+  id : 10
+};
+
+var token = jwt.sign(data, '123Abc');
+console.log(token);
+
+var decoded = jwt.verify(token, '123Abc');
+console.log('Decoded',decoded);
+// var message = 'I am user no 3';
+// var hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash : ${hash}`);
+//
+// var data = {
+//   id : 4
+// };
+//
+// var token = {
+//   data,
+//   hash : SHA256(JSON.stringify(data) + 'something').toString()
+// }
+//
+// // token.data.id = 5;
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+//
+// var resultHash = SHA256(JSON.stringify(token.data) + 'something').toString();
+//
+// if(resultHash === token.hash){
+//   console.log('data is not change');
+// } else {
+//   console.log('data is changed');
+// }
