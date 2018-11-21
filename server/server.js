b07c3110e48ba4db4159e5655ad6c2148d4d2f29
Arjun Vedak <arjun.vedak07@hotmail.com>
2018-11-21 08:28:57 +0530

    middle added for authentication

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
+
 var app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
@@ -114,6 +116,12 @@ app.post('/users', (req, res) => {
     .catch((e) => {
       res.status(400).send(e);
     });
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () =>{
