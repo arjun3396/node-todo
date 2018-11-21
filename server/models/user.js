db2e5cf87501866a87eca7ea5003a9701565bde1
Arjun Vedak <arjun.vedak07@hotmail.com>
2018-11-21 21:50:45 +0530

    update config to use json

--- server/models/user.js
+++ server/models/user.js
@@ -43,7 +43,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id : user._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({_id : user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
   user.tokens = user.tokens.concat([{access, token}]);
   return user.save()
@@ -57,7 +57,7 @@ UserSchema.methods.generateAuthToken = function () {
       var decoded = undefined;
 
       try{
-        decoded = jwt.verify(token, 'abc123')
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
       } catch (e) {
         // return new Promise((resolve, reject) => {
         //   reject();
